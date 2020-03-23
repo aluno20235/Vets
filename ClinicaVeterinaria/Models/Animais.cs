a57761761fbd6c376cab4ef67c319c3ae155cdff
Jaime Marques <aluno20235@ipt.pt>
2020-03-23 20:23:34 +0000

    Criação das tabelas

---
+++ ClinicaVeterinaria/Models/Animais.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinaria.Models
+{
+
+    /// <summary>
+    /// Classe representa a tabela dos 'Animais' na base de dados
+    /// </summary>
+    public class Animais
+    {
+        public int ID { set; get; }
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public float Peso { get; set; }
+        public string Raca { get; set; }
+        public string Foto { get; set; }
+            }
+}
---
+++ ClinicaVeterinaria/Models/Consultas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaVeterinaria.Models
+{
+    public class Consultas
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Observacoes { get; set; }
+
+    }
+}
