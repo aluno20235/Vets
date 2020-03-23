f5e201d3b968d9d13d6c3c6dce26fc8cbf006407
Jaime Marques <aluno20235@ipt.pt>
2020-03-23 21:57:31 +0000

    Criação da Base de dados

--- ClinicaVeterinaria/Startup.cs
+++ ClinicaVeterinaria/Startup.cs
@@ -6,6 +6,7 @@ using ClinicaVeterinaria.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
