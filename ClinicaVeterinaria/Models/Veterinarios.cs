de4054f09ac0ddee436e901270412c98d1f81963
Jaime Marques <aluno20235@ipt.pt>
2020-03-30 22:08:04 +0100

    Criação do controler + views de crud e validação dos dados

--- ClinicaVeterinaria/Models/Veterinarios.cs
+++ ClinicaVeterinaria/Models/Veterinarios.cs
@@ -15,8 +15,12 @@ namespace ClinicaVeterinaria.Models
 
         [Key]
         public int ID { get; set; }
+        [Required(ErrorMessage ="O Nome é de preenchimento obrigatório")]
+        [StringLength(40,ErrorMessage ="O {0} não deve ter mais de {1} carateres")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "A Cédula é de preenchimento obrigatório")]
         public string NumCedulaProf { get; set; }
+        [Required(ErrorMessage = "A Fotografia é de preenchimento obrigatório")]
         public string Foto { get; set; }
 
         //Lista de Consultas
