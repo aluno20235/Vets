1fb15968af03d72e92e541411eef8e7a8b3c8ef0
Jaime Marques <aluno20235@ipt.pt>
2020-04-06 21:22:36 +0100

    Adição de REGEX. Criação do Controler Donos

--- ClinicaVeterinaria/Models/Donos.cs
+++ ClinicaVeterinaria/Models/Donos.cs
@@ -15,7 +15,15 @@ namespace ClinicaVeterinaria.Models
 
         [Key]
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "O Nome é de preenchimento obrigatório")]
+        [StringLength(40, ErrorMessage = "O {0} não deve ter mais de {1} carateres")]
+        [RegularExpression("[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | d[ao](s)? |-|'| d')[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+){1,3}", ErrorMessage = "Deve introduzir o nome corretamente")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O NIF é de preenchimento obrigatório")]
+        [StringLength(9, ErrorMessage = "O {0} não deve ter mais de {1} carateres")]
+        [RegularExpression("[1-9][0-9]{8}", ErrorMessage = "Deve introduzir o NIF corretamente e deverá introduzir 9 algarismos.")]
         public string NIF { get; set; }
 
         //Lista dos animais que o Dono tem
--- ClinicaVeterinaria/Models/Veterinarios.cs
+++ ClinicaVeterinaria/Models/Veterinarios.cs
@@ -15,11 +15,20 @@ namespace ClinicaVeterinaria.Models
 
         [Key]
         public int ID { get; set; }
+
         [Required(ErrorMessage ="O Nome é de preenchimento obrigatório")]
         [StringLength(40,ErrorMessage ="O {0} não deve ter mais de {1} carateres")]
+        [RegularExpression("[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+(( | e | de | d[ao](s)? |-|'| d')[A-ZÁÉÍÓÚÂ][a-záéíóúàèìòùäëïöüãõâêîôûçñ]+){1,3}", ErrorMessage = "Deve introduzir o nome corretamente")]
         public string Nome { get; set; }
+
+
         [Required(ErrorMessage = "A Cédula é de preenchimento obrigatório")]
+        [RegularExpression("vet-[0-9]{6}", ErrorMessage ="Deve introduzir a palavra 'vet-' (em minúsculas), seguida de 6 dígitos.")]
+        [StringLength(10, ErrorMessage = "O {0} não deve ter mais de {1} carateres")]
+        [Display(Name ="Nº de Cédula Profissional")]
         public string NumCedulaProf { get; set; }
+
+
         [Required(ErrorMessage = "A Fotografia é de preenchimento obrigatório")]
         public string Foto { get; set; }
 
